ce2c494ea8c8ccdca06ded1fef3e3bf17238b307
Eilif Johansen <eilif@eilifjohansen.no>
2020-04-17 20:02:41 +0200

    Update register-service-worker.js

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -1,4 +1,4 @@
-import { register } from 'register-service-worker'
+import { register } from "register-service-worker";
 
 // The ready(), registered(), cached(), updatefound() and updated()
 // events passes a ServiceWorkerRegistration instance in their arguments.
@@ -11,31 +11,41 @@ register(process.env.SERVICE_WORKER_FILE, {
 
   // registrationOptions: { scope: './' },
 
-  ready (/* registration */) {
+  ready(/* registration */) {
     // console.log('Service worker is active.')
   },
 
-  registered (/* registration */) {
+  registered(/* registration */) {
     // console.log('Service worker has been registered.')
   },
 
-  cached (/* registration */) {
+  cached(/* registration */) {
     // console.log('Content has been cached for offline use.')
   },
 
-  updatefound (/* registration */) {
+  updatefound(/* registration */) {
     // console.log('New content is downloading.')
   },
 
-  updated (/* registration */) {
-    // console.log('New content is available; please refresh.')
+  updated(registration) {
+    // registration -> a ServiceWorkerRegistration instance
+    console.log("New content is available; please refresh.");
+    Notify.create({
+      message: i18n.t("messages.update_available"),
+      icon: "cloud_download",
+      closeBtn: i18n.t("labels.update"),
+      timeout: 10000,
+      onDismiss() {
+        location.reload(true);
+      }
+    });
   },
 
-  offline () {
+  offline() {
     // console.log('No internet connection found. App is running in offline mode.')
   },
 
-  error (/* err */) {
+  error(/* err */) {
     // console.error('Error during service worker registration:', err)
   }
-})
+});
