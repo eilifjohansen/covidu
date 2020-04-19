04a52b4cf200fc4141a4c604703d15eaf3938793
Eilif Johansen <eilif@eilifjohansen.no>
2020-04-20 01:57:44 +0200

    Update Chat.vue

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -95,6 +95,7 @@ export default {
   methods: {
     sendMessage() {
       this.$refs.newMessage.focus();
+
       if (this.newMessage.length >= 2 && this.newMessage.length <= 100) {
         this.messages.push({
           text: this.newMessage,
@@ -218,7 +219,6 @@ export default {
       this.clearMessage();
     },
     clearMessage() {
-      this.$refs.newMessage.focus();
       this.newMessage = "";
     },
     onLine() {
