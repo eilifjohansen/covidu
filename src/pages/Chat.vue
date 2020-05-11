0e6b4c3b9d4b4c25cc8992eae9658d41e9405e0c
Eilif Johansen <eilif@eilifjohansen.no>
2020-05-11 14:15:24 +0200

    Update Chat.vue

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -115,7 +115,7 @@ export default {
     getFocus() {
       setTimeout(() => {
         this.scrollToBottom();
-      }, 500);
+      }, 300);
     },
     sendMessage() {
       this.$refs.newMessage.focus();
